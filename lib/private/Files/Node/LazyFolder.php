2eac2d0b6400db97bf4fd9dace2be224b6f2072f
Arthur Schiwon <blizzz@arthur-schiwon.de>
2023-06-08 00:15:42 +0200

    feat: add searchBySystemTag as PHP API
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- lib/private/Files/Node/LazyFolder.php
+++ lib/private/Files/Node/LazyFolder.php
@@ -447,6 +447,10 @@ class LazyFolder implements \OCP\Files\Folder {
 		return $this->__call(__FUNCTION__, func_get_args());
 	}
 
+	public function searchBySystemTag(string $tagName, string $userId, int $limit = 0, int $offset = 0) {
+		return $this->__call(__FUNCTION__, func_get_args());
+	}
+
 	/**
 	 * @inheritDoc
 	 */
