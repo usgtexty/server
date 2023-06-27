fd5edf091ae0f9ffc53b75832fdb268261d6cdfa
Julius Härtl <jus@bitgrid.net>
2023-06-27 08:20:19 +0200

    debug: skip service worker registration
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- apps/files/src/services/ServiceWorker.js
+++ apps/files/src/services/ServiceWorker.js
@@ -23,6 +23,7 @@ import { generateUrl } from '@nextcloud/router'
 import logger from '../logger.js'
 
 export default () => {
+	return
 	if ('serviceWorker' in navigator) {
 		// Use the window load event to keep the page load performant
 		window.addEventListener('load', async () => {
