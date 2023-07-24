a70a85a5dcc9d52d2449d9c7ac0560a44592bfbd
Robin Appelman <robin@icewind.nl>
2023-07-24 16:35:54 +0200

    sso ci: better login check
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- apps/files_external/tests/start-apache.sh
+++ apps/files_external/tests/start-apache.sh
@@ -2,8 +2,9 @@
 
 docker rm -f apache 2>/dev/null > /dev/null
 
-docker run -d --name apache -v $2:/var/www/html -v /tmp/shared:/shared --dns $1 --hostname httpd.domain.test icewind1991/samba-krb-test-apache 1>&2
+docker run -d --name apache -v $2:/var/www/html -v /var/www/html/data -v /var/www/html/config -v /tmp/shared:/shared --dns $1 --hostname httpd.domain.test icewind1991/samba-krb-test-apache 1>&2
 APACHE_IP=$(docker inspect apache --format '{{.NetworkSettings.IPAddress}}')
+docker exec apache chown 33 /var/www/html/config /var/www/html/data
 
 # add the dns record for apache
 docker exec dc samba-tool dns add krb.domain.test domain.test httpd A $APACHE_IP -U administrator --password=passwOrd1 1>&2
